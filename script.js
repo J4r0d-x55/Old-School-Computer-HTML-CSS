edd62e3e8bbd2acee66b7dccabdba6e4b0de75d5
J4r0d-x55 <108584837+J4r0d-x55@users.noreply.github.com>
2022-09-07 09:42:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav_menu");
+
+hamburger.addEventListener("click", () =>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>{
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
